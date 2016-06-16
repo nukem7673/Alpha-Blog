94668b2666cf45288b851137c1c457beb98c92a1
Rails Udemy Course <richard.newcomb1@gmail.com>
2016-06-16 15:19:15 +0000

    Create rails application and setup home and about pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,19 @@
+class PagesController < ApplicationController
+    
+    def home
+        
+    end
+    
+    def about
+        
+    end
+    
+    def tucker
+        
+    end
+    
+    def esmae
+        
+    end
+    
+end
